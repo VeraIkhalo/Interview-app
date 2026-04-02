8f230dbcbccfc881c283145d57407c4551934edb
Vera Ikhalo <71321135+VeraIkhalo@users.noreply.github.com>
2026-04-02 14:19:21 +0100

    feat: add initial setup for React application with Vite
    - Added SVG assets for React and Vite logos.
    - Created global CSS variables and styles in index.css.
    - Implemented main entry point in main.jsx to render the App component.
    - Configured Vite with React plugin in vite.config.js.

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { ENV } from './lib/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ Msg: 'Success from api' });
+});
+
+const PORT = ENV.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
